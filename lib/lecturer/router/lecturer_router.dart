d298098f5052f9977e76b8c60112c5c022ec028e
niyiayooluwa <aytholu@gmail.com>
2026-05-13 05:03:37 +0100

    Remove unused lecturer and student router files

--- lib/lecturer/router/lecturer_router.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:go_router/go_router.dart';
-
-final lecturerRouteProvider = Provider<GoRouter>((ref) {
-  return GoRouter(initialLocation: '/', routes: []);
-});
